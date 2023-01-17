b3c442821221c73e2a8e2a1a8bef6c738ed19917
Eder <edermartelinho@hotmail.com>
2023-01-17 19:06:17 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,13 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		
+		//Programacao defensiva
+		
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error: there must be at least 1 row and 1 column");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -16,24 +23,40 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-
+	
+	/* retirado para manter intacto as linhas
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
+	*/
 
 	public int getColumns() {
 		return columns;
 	}
-
+	
+    /*retirado para manter intacto as colunas
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
+	*/
+	
+	
 	
 	public Piece piece(int row, int column) {
+		//Programacao defensiva
+		
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		//Programacao defensiva
+		
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 	  return pieces[position.getRow()][position.getColumn()] ;
 	
 	}
@@ -41,10 +64,30 @@ public class Board {
 	// Atribuicao da peca a posicao no tabuleiro
 	
 	public void placePiece(Piece piece, Position position) {
+		//Programacao defensiva
+		
+		if (thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position" + position);
+		}
 		 pieces[position.getRow()][position.getColumn()] = piece;
 		 piece.position = position;
 	}
 	
+	private boolean positionExists(int row, int column) {
+		return row >=0 && row < rows && column >= 0 && column < columns;
+	}
 	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
 	
+	public boolean thereIsAPiece(Position position) {
+		//Programacao defensiva
+		
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
+	}
+		
 }
