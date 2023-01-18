241c0da273efc8a0ad895e2c70331fc70a9f5e35
Eder <edermartelinho@hotmail.com>
2023-01-18 08:38:55 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,35 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
     //vamos instaciar uma partida de xadrez e imprimir o tabuleiro dessa partida
 		
 		ChessMatch chessMatch = new ChessMatch();
 		
 	// esta e a funcao para imprimir as pecas da partida
-	// criaremos uma classe UI(UserInterface)
-		
-		UI.printBoard(chessMatch.getPieces());
-		
-		
+	
+		while (true) {
+		    UI.printBoard(chessMatch.getPieces());
+		    System.out.println();
+		    System.out.print("Source: ");
+		    ChessPosition source = UI.readChessPosition(sc);
+		    
+		    System.out.println();
+		    System.out.print("Target: ");
+		    ChessPosition target = UI.readChessPosition(sc);
+		    
+		    ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,17 +1,12 @@
 package chess;
 
-public class ChessException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-<<<<<<< HEAD
+import boardgame.BoardException;
 
-	public ChessException(String msg) {
-		super(msg);
-	}
-=======
+public class ChessException extends BoardException {
+	private static final long serialVersionUID = 1L;
 	
 	public ChessException(String msg) {
 		super(msg);
 	}
 
->>>>>>> 734502d52f6787b06dda1cb3aa92c693aaff62d0
 }
